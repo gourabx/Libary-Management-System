6a31557a270907c68371246225ba04e209a30668
gourabx <gourabbanerjee926@gmail.com>
2025-07-22 20:24:49 +0530

    Update BookList.js

--- frontend/src/BookList.js
+++ frontend/src/BookList.js
@@ -3,13 +3,23 @@ import books from './books';
 
 function BookList() {
   return (
-    <div>
-      <h2>Library Book List</h2>
-      <div>
+    <div style={{ padding: '20px' }}>
+      <h2>Book List</h2>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
         {books.map((book, index) => (
-          <div key={index}>
+          <div
+            key={index}
+            style={{
+              border: '1px solid #ccc',
+              padding: '10px',
+              margin: '10px',
+              width: '200px',
+              textAlign: 'center'
+            }}
+          >
+            <img src={book.image} alt={book.name} style={{ width: '100px', height: '150px' }} />
             <h4>{book.name}</h4>
-            <p>By: {book.writer}</p>
+            <p>by {book.writer}</p>
           </div>
         ))}
       </div>
@@ -18,7 +28,6 @@ function BookList() {
 }
 
 export default BookList;
-
 
 
 
